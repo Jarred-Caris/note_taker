0ca95cc63970412f4b5a899bb43906e94b168c0a
jarred <jarred.caris@gmail.com>
2021-02-28 18:06:44 +1030

    app.get added and uniqid const

--- server.js
+++ server.js
@@ -3,17 +3,35 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require("path");
-const uniqid = require("iniqid")
+const uniqid = require("uniqid");
 
 let note = [];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
+app.get("/add", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/db/db.json"))
+);
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+
+  newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+  console.log(newNote);
+
+  note.push(newNote);
+  res.json(newNote);
+});
 
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
-  });
-  
+  console.log(`App listening on PORT: ${PORT}`);
+});
