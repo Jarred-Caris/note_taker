b3f1a2caafc6a6ccd0065a7301e62d7f90467e9f
jarred <jarred.caris@gmail.com>
2021-03-04 14:28:49 +1030

    read file coding

--- server.js
+++ server.js
@@ -1,37 +1,41 @@
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const express = require("express");
 const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require("path");
 const uniqid = require("uniqid");
+const db = "./db/db.json";
 
-let note = [];
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+let noteArray = [];
 
 app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-app.get("/add", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
+app.get("/api/notes", (req, res) => res.json(noteArray));
 
-app.get("/api/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/db/db.json"))
-);
-
-app.post("/api/notes", (req, res) => {
-  const newNote = req.body;
-
-  newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
-  console.log(newNote);
-
-  note.push(newNote);
-  res.json(newNote);
+app.post("*", (req, res) => {
+  fs.readFile(db, (err, data) => {
+    if (err) {
+      console.log("Uh-Oh. Something hasnt worked");
+    } else {
+      const newNote = req.body;
+      noteArray.JSON.parse(res);
+      newNote.id = uniqid();
+      noteArray.push(newNote);
+      console.log("Congrats on your new note");
+    }
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App listening on PORT: ${PORT}`);
+// });
