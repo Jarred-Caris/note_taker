dab840cc6c345f2b1038eb4e5da28d2989901a3c
jarred <jarred.caris@gmail.com>
2021-03-04 15:15:27 +1030

    changed some code structure as not working

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const db = "./db/db.json";
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-const express = require("express");
-const fs = require("fs");
-const app = express();
-const PORT = process.env.PORT || 8080;
-const path = require("path");
-const uniqid = require("uniqid");
-const db = "./db/db.json";
 
 let noteArray = [];
 
@@ -20,9 +17,16 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
 app.get("/api/notes", (req, res) => res.json(noteArray));
 
-app.post("*", (req, res) => {
+const fs = require("fs");
+const uniqid = require("uniqid");
+
+app.post("/api/notes", (req, res) => {
   fs.readFile(db, (err, data) => {
     if (err) {
       console.log("Uh-Oh. Something hasnt worked");
@@ -36,6 +40,12 @@ app.post("*", (req, res) => {
   });
 });
 
-// app.listen(PORT, () => {
-//   console.log(`App listening on PORT: ${PORT}`);
-// });
+
+// writefile code------->
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
